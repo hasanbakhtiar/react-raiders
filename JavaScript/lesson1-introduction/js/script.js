35e6462679ae755d5a729132e0ea7a2b1ec7ff3f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-12-21 17:14:19 +0400

    js enter

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,49 @@
+// Primitive Data types 
+// String
+var info = "Hello JavaScript";
+console.log(info);
+// Number
+var infoOne = 5;
+console.log(infoOne);
+// Boolean
+// var infoTwo = true;
+var infoTwo = false;
+console.log(infoTwo);
+
+// Composite Data types
+// Array
+var infoThree = ["Hello",10,true];
+console.log(infoThree);
+// Object
+var infoFour = {
+    name:"Arthur",
+    age:35,
+    old:false
+}
+console.log(infoFour);
+
+//Special Data types
+// undefined 
+var a ;
+console.log(a);
+// null
+var b = null;
+console.log(b);
+
+
+
+
+
+
+
+// single line
+
+/*more line 
+debugger;
+ console.log("JavaScript1");
+ console.log("JavaScript2");
+ console.laog("JavaScript3");
+ console.log("JavaScript4");
+ console.log("JavaScript5");
+ console.log("JavaScript6");
+ */
