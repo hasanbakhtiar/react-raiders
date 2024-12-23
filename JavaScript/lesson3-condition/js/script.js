a20663f33471ed164ebee2df91a80637db44d452
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-12-23 11:47:06 +0400

    contions

---
+++ JavaScript/lesson3-condition/js/script.js
@@ -0,0 +1,40 @@
+// if -> eger
+// else -> deyilse
+// else if -> deyilse eger
+
+
+// a=20;
+// if (a<10) {
+//     console.log(true);
+    
+// }else if(a>20){
+//     console.log("boom");
+    
+// }else if(a>10){
+//     console.log('yes');
+    
+// }else{
+//     console.log(false);
+    
+// }
+
+
+let time = 13;
+let user = "Hasan Bakhtiar"
+if (time>=5 && time<=11) {
+    console.log(user+ " good morning");
+}else if(time>12 && time<17){
+console.log(user+ " good afternoon");
+
+}else if(time>=17 && time<=21){
+console.log(user+ " good evening");
+
+}else if(time>22 && time<=23){
+    console.log(user+ " good night");
+    
+}else{
+    console.log("no time");
+    
+}
+
+
