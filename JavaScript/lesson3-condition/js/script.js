be2b7a7a2f04ab001f48d559518d8c96f6b2ba73
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-12-23 12:08:03 +0400

    contions

--- JavaScript/lesson3-condition/js/script.js
+++ JavaScript/lesson3-condition/js/script.js
@@ -19,22 +19,47 @@
 // }
 
 
-let time = 13;
-let user = "Hasan Bakhtiar"
-if (time>=5 && time<=11) {
-    console.log(user+ " good morning");
-}else if(time>12 && time<17){
-console.log(user+ " good afternoon");
+// let time = 13;
+// let user = "Hasan Bakhtiar"
+// if (time>=5 && time<=11) {
+//     console.log(user+ " good morning");
+// }else if(time>12 && time<17){
+// console.log(user+ " good afternoon");
 
-}else if(time>=17 && time<=21){
-console.log(user+ " good evening");
+// }else if(time>=17 && time<=21){
+// console.log(user+ " good evening");
 
-}else if(time>22 && time<=23){
-    console.log(user+ " good night");
+// }else if(time>22 && time<=23){
+//     console.log(user+ " good night");
     
-}else{
-    console.log("no time");
+// }else{
+//     console.log("no time");
     
+// }
+
+
+
+
+let info = prompt();
+let converInfo = parseInt(info);
+switch(info){
+    case "5":
+        console.log('this is 5');
+    break;
+    case 10:
+        console.log('this is 10');
+    break;
+    case "hello":
+        console.log('this is hello');
+        info=4
+        if (info<10) {
+            console.log('yes');
+            
+        }
+    break;
+    default:
+        console.log("this is default");
+    break;
+        
+        
 }
-
-
