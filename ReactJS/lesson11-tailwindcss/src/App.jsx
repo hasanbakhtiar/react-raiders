244824ce18639a8c7aee8bfdbe7665aa33c8c9b5
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-06-25 18:05:11 +0400

    lastest

---
+++ ReactJS/lesson11-tailwindcss/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <>
+      <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 mt-55">
+        <div>
+          <div class="text-xl font-medium text-black dark:text-white">
+            ChitChat
+          </div>
+          <p class="text-gray-500 dark:text-green-400">
+            You have a new message!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
